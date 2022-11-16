0b4e5b8a2ad4d4ecb85e73a4a1061b61e12eda4a
Pietro Bucker <pietro_177@hotmail.com>
2022-11-16 17:27:13 -0300

    implementando requisito 1

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,21 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(fetchProductsList).toBeInstanceOf(Function);
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    const expect = await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const expect = await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
-  // it('...', () => {
-  // });
-});
+  it('função fetchProductsListcom o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch', async () => {
+    const expect = await fetchProductsList('computador');
+    expect(expect).toEqual(computadorSearch);
+  });
+  });fetch
